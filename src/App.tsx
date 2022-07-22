5733a88522cbe4afbc12835fd15a5add673d0465
Emilly Landim <emyslandim@MacBook-Air-de-Emilly.local>
2022-07-22 00:32:37 -0300

    Create globalStyles with styled-component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import ProjectRoutes from './routes'
+import GlobaStyle from './styles/global'
+function App() {
+
+  return (
+    <div>
+      <GlobaStyle />
+      <ProjectRoutes />
+    </div>
+  )
+}
+
+export default App
